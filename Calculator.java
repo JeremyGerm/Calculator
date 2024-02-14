0077c896f73c24bdcf6eb1c6ba2e2c82723272f4
JeremyGerm <jeremyrwiening@gmail.com>
2024-02-14 13:47:57 -0500

    Asks user for two integers

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculator {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Please input two integers.");
+
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+
+        int num2 = scan.nextInt();
+    }
+}
